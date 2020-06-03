8c4dbf1fb053c031ffbbaba8ed51c47c157f15b1
lucien1011 <lucien1011@gmail.com>
2020-06-03 10:12:49 -0400

    update

--- EventBuilder.py
+++ EventBuilder.py
@@ -1,7 +1,11 @@
 import uproot
 
 class EventBuilder(object):
-    def build_single_file(self,inpath,treepath):
-        file = uproot.open(inpath)
-        tree = file[treepath] 
+    def build_single_file(self,in_path,tree_path):
+        file = uproot.open(in_path)
+        tree = file[tree_path] 
         return file,tree
+
+    def build_by_file_pattern(self,file_pattern,tree_path,branches,namedecode):
+        for path, file, start, stop, arrays in uproot.iterate(file_pattern,tree_path,branches,reportpath=True, reportfile=True, reportentries=True, namedecode=namedecode):
+            print(path, file, start, stop, len(arrays))
